f64870ef8a504ed2b53194a442fd56633fc1f777
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-02-21 02:53:26 -0500

    feat: LuaSnip with GoLang error handling smart snippet

--- lua/emnt-nvim/plugins.lua
+++ lua/emnt-nvim/plugins.lua
@@ -168,10 +168,7 @@ local plugins = {
     -- Snippets
     {
         "L3MON4D3/LuaSnip",
-        dependencies = {
-            -- Autocompletion
-            { "saadparwaiz1/cmp_luasnip" },
-        },
+        config = emntMod("snippets"),
     },
     -- Autocompletion
     {
@@ -179,6 +176,7 @@ local plugins = {
         dependencies = {
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-omni" },
+            { "saadparwaiz1/cmp_luasnip" },
             { "onsails/lspkind.nvim" },
         },
         config = emntMod("completion"),
