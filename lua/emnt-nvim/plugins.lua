833a60cd0ed87c160b3acbe511cde8ce8f6f37c6
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-06-26 03:54:05 -0400

    feat(lsp): Enable inlay hints

--- lua/emnt-nvim/plugins.lua
+++ lua/emnt-nvim/plugins.lua
@@ -100,6 +100,27 @@ local plugins = {
             { "hrsh7th/cmp-nvim-lsp" },
             -- JSON and YAML schemas for autocompletion
             { "b0o/schemastore.nvim" },
+            -- Support for LSP to insert virtual text, such as types for Rust
+            -- TODO: Remove this when hint support lands in nvim
+            {
+                "lvimuser/lsp-inlayhints.nvim",
+                config = function()
+                    local lsp_inlayhints = require("lsp-inlayhints")
+                    lsp_inlayhints.setup()
+                    vim.api.nvim_create_augroup("LspAttachInlayHints", {})
+                    vim.api.nvim_create_autocmd("LspAttach", {
+                        group = "LspAttachInlayHints",
+                        callback = function(args)
+                            if not (args.data and args.data.client_id) then
+                                return
+                            end
+                            local bufnr = args.buf
+                            local client = vim.lsp.get_client_by_id(args.data.client_id)
+                            lsp_inlayhints.on_attach(client, bufnr)
+                        end,
+                    })
+                end
+            },
         },
         config = function()
             require("emnt-nvim.lsp").setup()
